b3a44f4e16efda0777f55ad1d788b05f56f0ae55
Burko-Oleksandra <109693857+Burko-Oleksandra@users.noreply.github.com>
2022-10-20 14:02:38 +0000

    Deploying to gh-pages from @ Burko-Oleksandra/goit-js-hw-09@a3030491177dd3498834a5952d454005827e83ab 🚀

---
+++ 01-color-switcher.04e08d7a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");function n(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.disabled=!0,t.addEventListener("click",(function(){n(),timerId=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.04e08d7a.js.map
